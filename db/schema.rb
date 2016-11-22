455f5a4bb94b196a957c86947b369527b3770480
Louis Pouillot <louis@capsens.eu>
2016-11-22 18:30:05 +0100

    Change type of speech column in responses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122154844) do
+ActiveRecord::Schema.define(version: 20161122172719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20161122154844) do
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "speech"
+    t.text     "speech"
     t.text     "display_text"
     t.string   "data"
     t.string   "source"
