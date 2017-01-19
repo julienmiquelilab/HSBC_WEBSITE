f668ab21bfee202297c8b9ac8342239ebfa16be1
Louis Pouillot <louis.pouillot@gmail.com>
2017-01-19 15:33:20 +0100

    allow to modify token in admin dashboard

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register Client do
- permit_params :lastname, :firstname, :birthdate, :email, :login, :password, :agency_id, :advisor_id
+ permit_params :lastname, :firstname, :birthdate, :email, :login, :password, :agency_id, :advisor_id, :token
 
 end
