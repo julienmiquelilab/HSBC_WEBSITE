c4ea74a8965f6ac2281346d0962e88cecaa13f61
Louis Pouillot <louis.pouillot@gmail.com>
2017-01-25 18:16:29 +0100

    fix issue

--- app/admin/transfer.rb
+++ app/admin/transfer.rb
@@ -28,7 +28,7 @@ ActiveAdmin.register Transfer do
       f.input :amount
       f.input :description
       f.input :date, as: :datetime_picker
-      f.input :sender_account_id, as: :select, collection: account_collection, required: true
+      f.input :sender_account_id, as: :select, collection: account_collection
       f.input :receiver_account_id, as: :select, collection: account_collection, required: true
     end
     actions
@@ -41,7 +41,11 @@ ActiveAdmin.register Transfer do
           link_to resource.receiver_account.client.full_name, admin_client_path(resource)
         end
         row 'Sender' do |resource|
-          link_to resource.sender_account.client.full_name, admin_client_path(resource)
+          if resource.sender_account
+            link_to(resource.sender_account, admin_account_path(resource.sender_account))
+          else
+            "Unknown"
+          end
         end
         row :amount
         row :date
